4e88d7dcf01275449595b220cc0b82c748cb8cf0
syams <syamsrachman@gmail.com>
2018-05-26 00:22:09 +0700

    ngedit gambar anime & deskripsi

--- web/view/home.php
+++ web/view/home.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css"> -->
     <link rel="stylesheet" href="assets/bootstrap/css/sticky-footer.css">
     <link rel="stylesheet" href="assets/css/material-icons.css">
+    <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.css">
+    <link rel="stylesheet" href="assets/css/open-iconic.css">
+
 
     <title>Kepingan</title>
 </head>
@@ -14,31 +18,29 @@
 
 <?php include "web/elemen/navbar.php"; ?>
 
-<div class="jumbotron">
+<div class="container-fluid">
+  <div class="jumbotron">
     <h1 class="display-4">KEPINGAN</h1>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur eum excepturi
-        incidunt iste nam perferendis quasi vero voluptas! Aliquid blanditiis culpa dolorum fugit labore laboriosam nam
-        pariatur quod? Laudantium.</p>
-    <hr class="my-4">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Mulai</a>
+    <p class="lead">Simulasi Kerja Pabrik Susu Sapi Rembangan adalah sistem yang berisikan pengelolahan data sapi, data susu harian, dan
+      data penjualan. Sistem ini juga bisa meramalkan penjualan harian dengan metode Leastsquare</p>
+      <hr class="my-4">
+      <p>Pabrik Susu Sapi Rembangan</p>
+    </div>
 </div>
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-4 col-sm-12">
-            <img src="assets/gambar/ss.jpg" alt="" class="img-thumbnail float-left">
-            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque error iste itaque quasi vel
-                veritatis! Animi autem beatae distinctio ipsam iste non quis quos? At dolore eos maiores sint.</p>
+        <div class="col-md-4 col-sm-12 text-center">
+          <i class="material-icons" style="font-size:80px">book</i>
+            <p class="text-center">Sistem ini dapat mencatat data sapi, data produksi susu harian, dan data penjualan</p>
         </div>
         <div class="col-md-4 col-sm-12 text-center">
-            <img src="assets/gambar/ss.jpg" alt="" class="img-thumbnail">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque error iste itaque quasi vel
-                veritatis! Animi autem beatae distinctio ipsam iste non quis quos? At dolore eos maiores sint.</p>
+            <i class="material-icons" style="font-size:80px">lightbulb_outline</i>
+            <p>Dari data yang penjualan yang ada dapat diramalan penjualan selanjutnya dengan metode <i>Leastsquare</i></p>
         </div>
-        <div class="col-md-4 col-sm-12">
-            <img src="assets/gambar/ss.jpg" alt="" class="img-thumbnail float-right">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque error iste itaque quasi vel
-                veritatis! Animi autem beatae distinctio ipsam iste non quis quos? At dolore eos maiores sint.</p>
+        <div class="col-md-4 col-sm-12 text-center">
+          <i class="material-icons" style="font-size:80px">wb_sunny</i>
+            <p>Dengan adanya ramalan penjualan diharapkan pabrik dapat mengira ngira berapa distributor yang dibutuhkan untuk menyebarkan susu</p>
         </div>
     </div>
 </div>
