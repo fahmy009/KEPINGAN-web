3145379a044eee9369dff37f17c05ea3612c4469
fahmy009 <muhammadfahmy009@gmail.com>
2018-05-31 00:19:55 +0700

    tampilan akhir

--- web/view/tentang.php
+++ web/view/tentang.php
@@ -1,46 +1,103 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css"> -->
-    <link rel="stylesheet" href="assets/bootstrap/css/sticky-footer.css">
-    <link rel="stylesheet" href="assets/css/material-icons.css">
-    <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.css">
-    <link rel="stylesheet" href="assets/css/open-iconic.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
+  <title>Business Casual - Start Bootstrap Theme</title>
 
-    <title>Kepingan</title>
+  <!-- Bootstrap core CSS -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="assets/css/business-casual.min.css" rel="stylesheet">
+
 </head>
+
 <body>
 
-<?php include "web/elemen/navbar.php"; ?>
+  <h1 class="site-heading text-center text-white d-none d-lg-block">
+    <span class="site-heading-upper text-primary mb-3">Simulasi Kerja Pabrik Susu Sapi Rembangan</span>
+    <span class="site-heading-lower">KEPINGAN</span>
+  </h1>
 
-<div class="container-fluid">
-  <div class="jumbotron">
-    <h1 class="display-4">KEPINGAN</h1>
-    <p class="lead">
-      <h4>Anggota Kelompok : </h4>
-      <ul>
-        <li>KIKI RIZKI EMILIA (152410101039)</li>
-        <li>SYAMS RACHMAN W (152410101054)</li>
-        <li>M. FAHMY AMIRUN N. (152410101082)</li>
-        <li>GAVIN LIFFERA K P. (152410101096)</li>
-        <li>M. ZULFARULHAQ A (152410101129)</li>
-      </ul>
-    </p>
-      <hr class="my-4">
-      <p>Kelas B kelompok J</p>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="http://localhost/KEPINGAN/">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item active px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="http://localhost/KEPINGAN/?c=halaman&f=dataSapi">Data Sapi</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="http://localhost/KEPINGAN/?c=halaman&f=hasilSusuHarian">Hasil Susu Harian</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="http://localhost/KEPINGAN/?c=halaman&f=dataPenjualan">Data Penjualan Harian</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="http://localhost/KEPINGAN/?c=peramalanController&f=ramal">Ramalan Penjualan</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="http://localhost/KEPINGAN/?c=halaman&f=tentang">Tentang</a>
+          </li>
+        </ul>
+      </div>
     </div>
-</div>
+  </nav>
 
+  <section class="page-section about-heading">
+    <div class="container">
+      <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt="">
+      <div class="about-heading-content">
+        <div class="row">
+          <div class="col-xl-9 col-lg-10 mx-auto">
+            <div class="bg-faded rounded p-5">
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">KEPINGAN</span>
+                <span class="section-heading-lower">KELOMPOK SCM J</span>
+              </h2>
+              <ul>
+                <li>KIKI RIZKI EMILIA   (152410101039)</li>
+                <li>SYAMS RACHMAN W     (152410101054)</li>
+                <li>M. FAHMY AMIRUN N.  (152410101082)</li>
+                <li>GAVIN LIFFERA K P.  (152410101096)</li>
+                <li>M. ZULFARULHAQ A    (152410101129)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-<?php include "web/elemen/footer.php"; ?>
+  <footer class="footer text-faded text-center py-5">
+    <div class="container">
+      <p class="m-0 small">Copyright &copy; KEPINGAN 2018</p>
+    </div>
+  </footer>
 
-<script src="assets/bootstrap/js/jquery-3.3.1.slim.min.js"></script>
-<script src="assets/bootstrap/js/popper.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
